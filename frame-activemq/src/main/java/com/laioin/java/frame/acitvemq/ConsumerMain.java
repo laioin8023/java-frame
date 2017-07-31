59500e4af494f4bd7b2e9312bd0b3d9f413b05ee
qinxiancheng <qinxiancheng@ethank.com.cn>
2017-07-31 14:51:33 +0800

    init repertory

---
+++ frame-activemq/src/main/java/com/laioin/java/frame/acitvemq/ConsumerMain.java
@@ -0,0 +1,70 @@
+package com.laioin.java.frame.acitvemq;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jms.*;
+import java.io.ByteArrayOutputStream;
+
+/**
+ * Created with IDEA
+ * User qinxiancheng
+ * Date on 2017/7/31
+ * Time on 14:46
+ * activemq -- 消费者
+ */
+public class ConsumerMain {
+    private static Logger LGR = LoggerFactory.getLogger(ConsumerMain.class);
+
+    private static final String url = "tcp://192.168.1.222:61616";
+    private static final String QUEUE_NAME = "topic-ccs-ad";
+    private static final String userName = "ethankserver";
+    private static final String pwd = "serverethank";
+
+    public static void main(String[] args) throws Exception {
+        // 构造ConnectionFactory实例对象，此处采用ActiveMq的实现jar
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(userName, pwd, url);
+        ActiveMQQueue queue = new ActiveMQQueue(QUEUE_NAME);
+
+        Connection cnn = connectionFactory.createConnection();
+        cnn.start();
+
+        Session session = cnn.createSession(false, 1);
+        MessageConsumer consumer = session.createConsumer(queue);
+        consumer.setMessageListener(new MessageListener() {
+            @Override
+            public void onMessage(Message message) {
+                String msg = null;
+                if (message instanceof TextMessage) {
+                    TextMessage tm = (TextMessage) message;
+                    try {
+                        msg = tm.getText();
+                    } catch (JMSException e) {
+                        LGR.error("get text from message failed.", e);
+                    } catch (Throwable t) {
+                        LGR.error("unknown error occured when get text from message.", t);
+                    }
+                } else if (message instanceof BytesMessage) {
+                    try {
+                        BytesMessage bm = (BytesMessage) message;
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        byte[] b = new byte[1024 * 4];
+                        for (int c = bm.readBytes(b); c > 0; c = bm.readBytes(b)) {
+                            baos.write(b, 0, c);
+                        }
+                        msg = baos.toString();
+                    } catch (Exception e) {
+                        LGR.error("failed to convert BytesMessage to String.", e);
+                    }
+                } else {
+                    LGR.warn("unknown type of message received. message:" + message);
+                    msg = message.toString();
+                }
+                System.out.println(msg);
+            }
+        });
+
+    }
+}
---
+++ frame-activemq/src/main/java/com/laioin/java/frame/acitvemq/ProducerMain.java
@@ -0,0 +1,52 @@
+package com.laioin.java.frame.acitvemq;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
+
+import javax.jms.*;
+
+/**
+ * Created with IDEA
+ * User qinxiancheng
+ * Date on 2017/7/31
+ * Time on 14:46
+ * activemq -- 生产者
+ */
+public class ProducerMain {
+    private static final String url = "tcp://192.168.1.222:61616";
+    private static final String QUEUE_NAME = "topic-ccs-ad";
+    private static final String userName = "ethankserver";
+    private static final String pwd = "serverethank";
+
+    public static void sendMessage() throws Exception {
+        try {
+            // 构造ConnectionFactory实例对象，此处采用ActiveMq的实现jar
+            ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(userName, pwd, url);
+            ActiveMQQueue queue = new ActiveMQQueue(QUEUE_NAME);
+
+            Connection cnn = connectionFactory.createConnection();
+            cnn.start();
+
+            Session session = cnn.createSession(false, 1);
+            int size = 1000;
+            long s = System.currentTimeMillis();
+            for (int i = 0; i < size; i++) {
+                String msg = "大多数消息系统声称可以做到“精确的一次”，但是仔细阅读它们的的文档可以看到里面存在误导，比如没有说明当consumer或producer失败时怎么样，或#" + i;
+                TextMessage tm = session.createTextMessage(msg);
+                MessageProducer pdc = session.createProducer(queue);
+                pdc.send(tm, 2, Message.DEFAULT_PRIORITY, Message.DEFAULT_TIME_TO_LIVE);
+            }
+            System.out.println("send 200K size " + size + "  " + (System.currentTimeMillis() - s));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            ProducerMain.sendMessage();
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+        }
+    }
+}
